7b258d30510c715ef3db8bb905d0a2467a64987c
Eduardo <eduabarraque@gmail.com>
2022-06-07 11:11:21 -0300

    up to desktop

--- produtos/models.py
+++ produtos/models.py
@@ -17,7 +17,7 @@ class PedidoItem(models.Model):
 
 class Produto(models.Model):
     nome = models.CharField(max_length=50)
-    quant_estoque = models.IntegerField()
-    preco_unit = models.DecimalField(decimal_places=2, max_digits= 8)
-    imagem = models.URLField(max_length=300)
+    quant_estoque = models.IntegerField(null=True, blank=True)
+    preco_unit = models.DecimalField(decimal_places=2, max_digits=8, null=True, blank=True)
+    imagem = models.URLField(max_length=300, null=True, blank=True)
     ativo = models.BooleanField()
--- produtos/views.py
+++ produtos/views.py
@@ -14,7 +14,7 @@ def adicionar(request):
     if request.method == "POST":
         produto = Produto()
         produto.nome = request.POST.get('nome', None)
-        produto.quant_estoque = int(request.POST.get('quant_estoque', None))
+        produto.quant_estoque = request.POST.get('quant_estoque', None)
         produto.preco_unit = request.POST.get('preco_unit', None)
         produto.imagem = request.POST.get('imagem', None)
         produto.ativo = True
